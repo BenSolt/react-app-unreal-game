23d56092baf01b708b4d39dc34e733cb5a326c7c
benjamin-solt <bensolt@gmail.com>
2021-07-20 10:06:49 -0400

    changed layout of page looks better

--- src/AppUnrealGame.js
+++ src/AppUnrealGame.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import './AppUnrealGame.css';
 
+import Image1 from "../src/images/BlakeQuest_Image1.png"
+import Image2 from "../src/images/BlakeQuest_Image2.png"
+import Image3 from "../src/images/BlakeQuest_Image3.png"
+import Image4 from "../src/images/BlakeQuest_Image4.png"
+import Title from "../src/images/BlakesQuestBigCoverArt.png"
+
 function UnrealGame() {
 
   function openGame() {
@@ -8,33 +14,83 @@ function UnrealGame() {
     window.open("https://blakesquestgame.s3.us-east-2.amazonaws.com/BlakesAdventure.html");
   }
 
+  const Click1 = () => {
+    console.log('click')
+    document.getElementById("img01").src = Image1;
+    //element.src;
+    document.getElementById("myModal").style.display = 'block'
+  }
+
+  const Click2 = () => {
+    document.getElementById("img01").src = Image2;
+    //element.src;
+    document.getElementById("myModal").style.display = 'block'
+  }
+
+  const Click3 = () => {
+    document.getElementById("img01").src = Image3;
+    //element.src;
+    document.getElementById("myModal").style.display = 'block'
+  }
+
+  const Click4 = () => {
+    document.getElementById("img01").src = Image4;
+    //element.src;
+    document.getElementById("myModal").style.display = 'block'
+  }
+
+  window.onclick = function (e) {
+    if (e.target === document.getElementById("myModal")) {
+      document.getElementById("myModal").style.display = "none";
+    }
+  }
+
+  const ClickClose = () => {
+    document.getElementById("myModal").style.display = "none";
+  }
 
   return (
     <div className='unrealGamePage'>
       <div className="title">
-        <h1>Blake's Quest</h1>
+        <img className="imageGameTitle" src={Title} alt="game title" />
+        <div className="titleText">
+          <h1>Blake's Quest</h1>
+        </div>
       </div>
 
       <div className='sectionTop'>
-        <div>
-          <img className="imageGameTitle" src="BlakesQuestBigCoverArt.png" alt="screen shot of game" />
-        
+
+        <div className='sectionTopA'>
+          <h4> Created using Unreal Engine via Blueprints, Adobe Flash: for artwork, AWS: to host server</h4>
+          <h4>Click <button className="btnGame" onClick={openGame}>Load Game</button> to play</h4>
+          <h5>Side scroller game where you earn new abilities as you progress through the game, which enables you to explore new
+            locations. Learn abilities like double jump, wall climb, and even a jetpack</h5>
         </div>
 
-        <div>
-          <h3> Created using Unreal Engine via Blueprints, Adobe Flash: for artwork, AWS: to host server</h3>
-          <h3>Click <button className="btnGame" onClick={openGame}>Load Game</button> to play</h3>
-          <h4>Side scroller game where you obtain new abilities as you progress through the game, that enable you to explore new
-            locations. like double jump, wall climb, and even a jetpack</h4>
+        <div className='sectionTopB'>
+          <iframe className='videoSize'
+            title="Blakes Quest"
+            src="https://www.youtube.com/embed/fhSUHRPf9aA"
+            frameborder="0" allow="accelerometer; autoplay; 
+                    encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+          </iframe>
         </div>
-
       </div>
 
-      <div className='imageHolder'>
-        <img className="imageGame" src="BlakeQuest_Image1.png" alt="blake in start room" />
-        <img className="imageGame" src="BlakeQuest_Image2.png" alt="Blake with jetpack" />
-        <img className="imageGame" src="BlakeQuest_Image3.png" alt="Blake in science lab" />
-        <img className="imageGame" src="BlakeQuest_Image4.png" alt="Blake at store" />
+      <div id="myModal" className="modal">
+        <div class="modal-content">
+          <img id='img01' className='myImgsModal' onClick={ClickClose} alt='current image' />
+        </div>
+      </div>
+
+
+
+      <div className='sectionImages'>
+        <img className="imageGame" onClick={Click1} src={Image1} alt="blake in start room" />
+        <img className="imageGame" onClick={Click2} src={Image2} alt="Blake with jetpack" />
+        <img className="imageGame" onClick={Click3} src={Image3} alt="Blake in science lab" />
+        <img className="imageGame" onClick={Click4} src={Image4} alt="Blake at store" />
       </div>
 
     </div>

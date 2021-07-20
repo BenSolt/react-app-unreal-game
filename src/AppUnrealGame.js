841a6951fb7bcc6b2dcea9e0d6935a9b1194d0b0
benjamin-solt <bensolt@gmail.com>
2021-07-20 08:09:57 -0400

    editing format

--- src/AppUnrealGame.js
+++ src/AppUnrealGame.js
@@ -1,12 +1,7 @@
 import React, { useState, useEffect } from "react";
 import './AppUnrealGame.css';
 
-
-
 function UnrealGame() {
-
-  const [unrealgame, setUnrealGame] = useState([]);
-
 
   function openGame() {
 
@@ -17,7 +12,7 @@ function UnrealGame() {
   return (
     <div className='unrealGamePage'>
       <div className="title">
-        <h1>Blake's Quest - Unreal Engine Game</h1>
+        <h1>Blake's Quest</h1>
       </div>
 
       <div className='sectionTop'>
@@ -27,7 +22,7 @@ function UnrealGame() {
         </div>
 
         <div>
-          <h3> Created Via Blueprints, Adobe Flash: for artwork, AWS: to host server</h3>
+          <h3> Created using Unreal Engine via Blueprints, Adobe Flash: for artwork, AWS: to host server</h3>
           <h3>Click <button className="btnGame" onClick={openGame}>Load Game</button> to play</h3>
           <h4>Side scroller game where you obtain new abilities as you progress through the game, that enable you to explore new
             locations. like double jump, wall climb, and even a jetpack</h4>
@@ -36,10 +31,10 @@ function UnrealGame() {
       </div>
 
       <div className='imageHolder'>
-        <img className="imageGame" src="BlakeQuest_Image1.png" alt="screen shot of game" />
-        <img className="imageGame" src="BlakeQuest_Image2.png" alt="screen shot of game" />
-        <img className="imageGame" src="BlakeQuest_Image3.png" alt="screen shot of game" />
-        <img className="imageGame" src="BlakeQuest_Image4.png" alt="screen shot of game" />
+        <img className="imageGame" src="BlakeQuest_Image1.png" alt="blake in start room" />
+        <img className="imageGame" src="BlakeQuest_Image2.png" alt="Blake with jetpack" />
+        <img className="imageGame" src="BlakeQuest_Image3.png" alt="Blake in science lab" />
+        <img className="imageGame" src="BlakeQuest_Image4.png" alt="Blake at store" />
       </div>
 
     </div>

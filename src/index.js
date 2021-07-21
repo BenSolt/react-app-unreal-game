64c7594228a15453043db8cc0caa71474aefd8c3
benjamin-solt <bensolt@gmail.com>
2021-07-21 15:39:43 -0400

    removed app js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
 import AppUnrealGame from './AppUnrealGame';
 import * as serviceWorker from './serviceWorker';
 
